ffbd150b54c64b6f7659aae92c253dae2605ebba
seanmoran526 <36112653+seanmoran526@users.noreply.github.com>
2019-04-07 08:25:23 -0400

    Update compileOS.sh

--- compileOS.sh
+++ compileOS.sh
@@ -1,7 +1,13 @@
 dd if=/dev/zero of=floppya.img bs=512 count=2880
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
-dd if=map of=floppya.img bs=512 count=1 seek=256 conv=notrunc
-dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc
+dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
+dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
 bcc -ansi -c -o kernel.o kernel.c
-ld86 -o kernel -d kernel.o kasm.o
-dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel.o kernel_asm.o
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+bcc -ansi -c -o Shell.o Shell.c
+as86 lib.asm -o lib_asm.o
+ld86 -o Shell -d Shell.o lib_asm.o
+gcc -o loadFile loadFile.c
+./loadFile Shell
