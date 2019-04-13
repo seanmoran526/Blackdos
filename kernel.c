71deabbb753447e19559d382c62e6423f2bb66d0
seanmoran526 <36112653+seanmoran526@users.noreply.github.com>
2019-04-13 01:50:58 -0400

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -49,10 +49,7 @@ int mod(int, int);
 int div(int, int);
 void readSector(char*, int);
 void writeSector(char*,int);
-int strCmp(char*, char*, int);
-int findFile(char*, char*);
-int openDirIndex(char*);
-int openSector(char*);
+int strCmp(char*, char*);
 void clearScreen(int,int);
 void error(int);
 void readFile(char*, char*, int*);
@@ -68,7 +65,11 @@ void main()
  interrupt(33,2,buffer,258,0);
  interrupt(33,12,buffer[0]+1,buffer[1]+1,0);
  printLogo();
- interrupt(33,PRINTSTR,"Before Shell Call \r\n\0",0,0);
+ interrupt(33,PRINTSTR,"\r\n\r\n\0",0,0);
+ interrupt(33,PRINTSTR,"Press Enter to Begin: \0",0,0);
+ interrupt(33,1,buffer,0,0);
+ interrupt(33,12,0,0,0);
+ interrupt(33,PRINTSTR,"Welcome to Blackdos \r\n\0",0,0);
  interrupt(33,4, "Shell\0" ,2,0);
  interrupt(33,0,"Bad or missing command interpreter.\r\n\0",0,0);
  while (1) ;
@@ -221,191 +222,204 @@ void clearScreen(int back,int fore)
     }
 }
 
-int strCmp(char* a, char* b, int length)
+int strCmp(char* a, char* b)
 {
-    int i;
-    for(i=0; i<length && b[i]!='\0'; ++i)
+  int i = 0;
+  int boolFlag = 1;
+  while(a[i] != '\0')
+  {
+    if(a[i] != b[i])
     {
-        if(a[i] != b[i])
-        {
-            return 0;
-        }
+      boolFlag = 0;
+      break;
     }
-    return 1;
+    ++i;
+  }
+  return boolFlag;
 }
 
-int openSector(char* map)
-{
-    int i;
-    for(i=0; i<512; ++i)
-    {
-        if(map[i] == 0x00)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-
-int openDirIndex(char* dir)
-{
-    int i, j;
-    for(i=0; i<512; i+=32)
-    {
-        if(dir[i]=='\0')
-        {
-            for(j=1; j<8; ++j)
-            {
-                dir[i+j]= '\0';
-            }
-            return i;
-        }
-    }
-    return -1;
-}
-
-int findFile(char* dir, char* fname)
-{
-    int i;
-    for(i=0; i<512; i+=32)
-    {
-         if(strCmp(&dir[i],fname,8))
-         {
-             return i;
-         }
-    }
-    return -1;
-}
-
-void readFile(char* fname, char* buffer, int* size)
+void readFile(char* fname, char* buffer, char* size)
 {
     char dir[512];
-    int dirIndex, bufIndex;
-    *size=0;
-    readSector(dir,257);
-    dirIndex = findFile(dir, fname);
-    if(dirIndex==-1)
+    int i;
+    int dirIndex = 0;
+    int found =1;
+    int bufIndex = 0;
+    int boolFlag;
+    readSector(dir, 257);
+    while(bufindex < 512)
     {
-        interrupt(33,15,0,0,0);
-        return;
+      BoolFlag = strCmp(fname, &dir[bufindex]);
+      if(boolFlag == 1)
+      {
+          dirIndex = bufindex + 8;
+          for(dirIndex ; dir[dirIndex] != 0; ++dirIndex)
+          {
+              *size += 1;
+              readSector(buffer, dir[dirIndex]);
+              buffer += 512;
+          }
+          return;
+      }
+      bufIndex += 32;
     }
-    dirIndex+=8;
-    for(bufIndex=0; bufIndex<12288; bufIndex+=512)
-    {
-        if(dir[dirIndex]>0)
-        {
-           interrupt(33,PRINTSTR,"Sector Read. \r\n\0",0,0); 
-           interrupt(33,2,&buffer[bufIndex], dir[dirIndex],0);
-           ++dirIndex;
-        }
-        else
-        {
-           *size=bufIndex;
-           interrupt(33,PRINTSTR,"Hit Zero. \r\n\0",0,0);
+    interrupt(33,15,0,0,0);
+}
+
+void writeFile(char* fname,char* buffer,int numSect)
+{
+  char dir[512];
+  char map[512];
+  int boolFlag;
+  int dirIndex = 0;
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int l = 0;
+  int m = 0;
+  int free = 0;
+  readSector(dir, 257);
+  readSector(map, 256);
+   while(i < 17)
+   {
+       if(dir[m] == 0x0 )
+       {
+           ++free;
+       }
+       m += 32;
+       i += 1;
+   }
+   if(free == 0)
+   {
+       interrupt(33, 15, 3, 0, 0);
+       return;
+   }
+   while(dirIndex < 512)
+   {
+       boolFlag = strCmp(fname, &dir[dirIndex]);
+       if(dir[dirIndex] != 0x0 && boolFlag == 1 )
+       {
+           interrupt(33, 15, 1, 0, 0);
            return;
-        }
-    }
+       }
+       else if(dir[dirIndex] == 0)
+       {
+           break;
+       }
+       dirIndex += 32;
+   }
+   while(fname[j] != '\0')
+   {
+       dir[dirIndex + j] = fname[j];
+       ++j;
+   }
+
+   for(j = dirIndex; j < (dirIndex + 8); ++j)
+   {
+       dir[j] == 0;
+   }
+   dirIndex += 8;
+
+   for(j = 0; j < numSect; ++j)
+   {
+         while(k <  512)
+         {
+           if(map[k] == 0)
+           {
+               map[k] = 255;
+               dir[dirIndex + j] = k;
+
+               writeSector(buffer, k);
+               break;
+           }
+         ++k;
+       }
+   }
+   writeSector(dir, 257);
+   writeSector(map, 256);
 }
-
-void writeFile(char* fname, char* buffer, int numSect)
-{
-    char dir[512];
-    char map[512];
-    int dirIndex, mapIndex, j;
-    int i=0;
-    interrupt(33,2,dir,257,0);
-    interrupt(33,2,map,256,0);
-    do
-    {
-        if(findFile(dir, fname)!=-1)
-        {
-            interrupt(33,15,1,0,0);
-            return;
-        }
-        dirIndex=openDirIndex(dir);
-        mapIndex = openSector(map);
-        if(mapIndex==-1 || dirIndex==-1)
-        {
-            interrupt(33,15,2,0,0);
-            return;
-        }
-        map[mapIndex] = 0xFFFF;
-        dir[dirIndex+8+i]= mapIndex + 1;
-        dir[dirIndex+9+i]= '\0';
-        for(j=0; j<8 && fname[j]!='\0'; ++j)
-        {
-            dir[dirIndex+j] = fname[j];
-        }
-        interrupt(33, 6, buffer, mapIndex+1, 0);
-        buffer+=512;
-        ++i;
-    }while(i<numSect);
-    interrupt(33, 6, map, 256, 0);
-    interrupt(33, 6, dir, 257, 0);
-    return;
-}
-
-
 
 void deleteFile(char* fname)
 {
-    char dir[512];
-    char map[512];
-    int dirIndex, mapIndex, i;
-    interrupt(33,2,dir,257,0);
-    interrupt(33,2,map,256,0);
-    dirIndex = findFile(dir, fname);
-    if(dirIndex==-1)
-    {
-        interrupt(33,15,1,0,0);
-        return;
-    }
-    dir[dirIndex] = 0x0000;
-    dirIndex+=8;
-    for(i=0; i<24 && dir[dirIndex]!=0; ++i)
-    {
-        mapIndex = dir[dirIndex+i] - 1;
-        map[mapIndex] = 0x0000;
-    }
-    interrupt(33, 6, map, 256, 0);
-    interrupt(33, 6, dir, 257, 0);
-    return;
+   char dir[512];
+   char map[512];
+   int boolFlag;
+   int i = 0;
+   int j = 0;
+   int bufIndex = 0;
+   int mapIndex = 0;
+   readSector(dir, 257);
+   readSector(map, 256);
+   while(i<16)
+   {
+     boolFlag = strCmp(fname, &dir[bufIndex]);
+     if(boolFlag == 1)
+     {
+         for(j; j < 8; ++j)
+         {
+             dir[bufIndex + j] = 0;
+         }
+         for(j = bufIndex + 8; dir[j] != 0 ;++j)
+         {
+             mapIndex = dir[j];
+             dir[j] = 0;
+             map[mapIndex] = 0;
+         }
+
+         writeSector(dir, 257);
+         writeSector(map, 256);
+         return;
+     }
+     ++i;
+   }
+   interrupt(33, 15,0, 0, 0);
 }
 
-void runProgram(char* fname, int segment)
-{
-    char buffer[13312];
-    int offset, base, size;
-    if(segment<0 || segment>9)
+  void runProgram(char* fname, int segment)
+  {
+    char buffer[4000];
+    int offset=0;
+    readFile(fname, buffer, 1);
+    for(offset = 0 ; offset<4000 ; ++offset)
     {
-        interrupt(33,15,4,0,0);
-        return;
+        putInMemory(segment*4096, offset, buffer[offset]);
     }
-    base = segment*4096;
-    interrupt(33,PRINTSTR,"Before readFile \r\n\0",0,0);
-    interrupt(33,3,fname,buffer,&size);
-    interrupt(33,PRINTSTR,"after readFile \r\n\0",0,0);
-    for(offset=0; offset<size; ++offset)
-    {
-        putInMemory(base, offset, buffer[offset]);
-    }
-    interrupt(33,PRINTSTR,"Before Launch \r\n\0",0,0);
-    launchProgram(base);
-}
+    launchProgram(segment*4096);
+  }
+
 
 void stop(){launchProgram(8192);}
 
 void error(int bx)
 {
-    switch(bx)
-    {
-        case 0: interrupt(33,PRINTSTR,"File not found. \r\n\0",0,0); break;
-        case 1: interrupt(33,PRINTSTR,"Bad file name. \r\n\0",0,0); break;
-        case 2: interrupt(33,PRINTSTR,"Disk full \r\n\0",0,0); break;
-        default: interrupt(33,PRINTSTR,"General error \r\n\0",0,0);
-    }
-    return;
+   char errMsg0[18], errMsg1[17], errMsg2[13], errMsg3[17];
+
+   errMsg0[0] = 70; errMsg0[1] = 105; errMsg0[2] = 108; errMsg0[3] = 101;
+   errMsg0[4] = 32; errMsg0[5] = 110; errMsg0[6] = 111; errMsg0[7] = 116;
+   errMsg0[8] = 32; errMsg0[9] = 102; errMsg0[10] = 111; errMsg0[11] = 117;
+   errMsg0[12] = 110; errMsg0[13] = 100; errMsg0[14] = 46; errMsg0[15] = 13;
+   errMsg0[16] = 10; errMsg0[17] = 0;
+   errMsg1[0] = 66; errMsg1[1] = 97; errMsg1[2] = 100; errMsg1[3] = 32;
+   errMsg1[4] = 102; errMsg1[5] = 105; errMsg1[6] = 108; errMsg1[7] = 101;
+   errMsg1[8] = 32; errMsg1[9] = 110; errMsg1[10] = 97; errMsg1[11] = 109;
+   errMsg1[12] = 101; errMsg1[13] = 46; errMsg1[14] = 13; errMsg1[15] = 10;
+   errMsg1[16] = 0;
+   errMsg2[0] = 68; errMsg2[1] = 105; errMsg2[2] = 115; errMsg2[3] = 107;
+   errMsg2[4] = 32; errMsg2[5] = 102; errMsg2[6] = 117; errMsg2[7] = 108;
+   errMsg2[8] = 108; errMsg2[9] = 46; errMsg2[10] = 13; errMsg2[11] = 10;
+   errMsg2[12] = 0;
+   errMsg3[0] = 71; errMsg3[1] = 101; errMsg3[2] = 110; errMsg3[3] = 101;
+   errMsg3[4] = 114; errMsg3[5] = 97; errMsg3[6] = 108; errMsg3[7] = 32;
+   errMsg3[8] = 101; errMsg3[9] = 114; errMsg3[10] = 114; errMsg3[11] = 111;
+   errMsg3[12] = 114; errMsg3[13] = 46; errMsg3[14] = 13; errMsg3[15] = 10;
+   errMsg3[16] = 0;
+
+   switch(bx) {
+   case 0: printString(errMsg0); break;
+   case 1: printString(errMsg1); break;
+   case 2: printString(errMsg2); break;
+   default: printString(errMsg3);
+   }
 }
 
 
@@ -429,5 +443,5 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
       case 15: error(bx); break;
       default: printString("General BlackDOS error.\r\n\0", 0);
    }
-   return;
+   stop();
 }
